c2553d8dd145ceba6abe7caf091136e260d419d5
alberand <albershteyn.andrey@gmail.com>
2021-03-14 15:19:56 +0100

    fix: print something on serial

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
+#include <sys/printk.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -44,6 +45,7 @@ void main(void)
 	}
 
 	while (1) {
+	    printk("Hello World!\n");
 		gpio_pin_set(dev, PIN, (int)led_is_on);
 		led_is_on = !led_is_on;
 		k_msleep(SLEEP_TIME_MS);
